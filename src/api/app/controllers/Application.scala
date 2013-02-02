d1251b537c80db8606af6f22ba880720e04f4c54
James Hughes <james@yobriefca.se>
2013-02-02 11:54:37 +0000

    Added more trello list info and useful links

--- src/api/app/controllers/Application.scala
+++ src/api/app/controllers/Application.scala
@@ -1,12 +1,19 @@
 package controllers
 
 import play.api._
+import libs.json.Json
 import play.api.mvc._
 
 object Application extends Controller {
-  
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    render {
+      case Accepts.Json => Ok(Json.obj("success" -> true))
+      case Accepts.Xml => Ok(
+        <response>
+          <success>true</success>
+        </response>)
+    }
   }
-  
+
 }
