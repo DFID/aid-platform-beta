8fb4a6eda3c787ae2dd7800fcd433a6180c829c3
James Hughes <james@yobriefca.se>
2013-03-04 14:08:23 +0000

    Moved to standrard OSM tiles

--- src/platform/site/source/javascripts/leaflet/countrymap.js
+++ src/platform/site/source/javascripts/leaflet/countrymap.js
@@ -1,6 +1,6 @@
 (function(global, undefined){
     // Configuration items which could be moved to CMS
-    var mapDataUrlTemplate = 'http://{s}.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png';
+    var mapDataUrlTemplate = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     var initialZoomLevel = 5;
     var mapTileSourceSubdomains = 'abc';
 
