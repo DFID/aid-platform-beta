973ac996e31d6a5231820135c412ae9a1b9a0715
James Hughes <james@yobriefca.se>
2013-03-05 14:33:08 +0000

    Added ability to see what documents belong in the site

--- src/platform/site/config.rb
+++ src/platform/site/config.rb
@@ -41,11 +41,13 @@ end
 #------------------------------------------------------------------------------
 @cms_db['projects'].find({}).each do |project|
 
+  documents           = @cms_db['documents'].find({ 'project' => project['iatiId'] }).to_a
   funded_projects     = @cms_db['funded-projects'].find({ 'funding' => project['iatiId'] }).to_a
   has_funded_projects = funded_projects.size > 0
 
+
   proxy "/projects/#{project['iatiId']}/index.html",              '/projects/summary.html',      :locals => { :project => project, :has_funded_projects => has_funded_projects }
-  proxy "/projects/#{project['iatiId']}/documents/index.html",    '/projects/documents.html',    :locals => { :project => project, :has_funded_projects => has_funded_projects }
+  proxy "/projects/#{project['iatiId']}/documents/index.html",    '/projects/documents.html',    :locals => { :project => project, :has_funded_projects => has_funded_projects, :documents => documents }
   proxy "/projects/#{project['iatiId']}/transactions/index.html", '/projects/transactions.html', :locals => { :project => project, :has_funded_projects => has_funded_projects }
 
   if has_funded_projects then
