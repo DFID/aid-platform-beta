90f665eeec00745f2c482ee4600f2c45a537f005
James Hughes <james@yobriefca.se>
2013-03-14 15:58:15 +0000

    Tidied up commits

--- src/platform/app/lib/JsonWriters.scala
+++ src/platform/app/lib/JsonWriters.scala
@@ -6,9 +6,6 @@ import play.api.libs.json.JsString
 import play.api.libs.json.JsBoolean
 import play.api.libs.json.JsNumber
 import collection.JavaConversions._
-import uk.gov.dfid.loader.Implicits._
-import org.neo4j.cypher.internal.symbols.RelationshipType
-import org.json.JSONArray
 
 object JsonWriters {
 
