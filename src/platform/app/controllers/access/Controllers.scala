0ad7083230d7b1c00ba6efb53a78c455ebd874b6
James Hughes <james@yobriefca.se>
2013-03-20 20:10:42 +0000

    some more api wokr

--- src/platform/app/controllers/access/Controllers.scala
+++ src/platform/app/controllers/access/Controllers.scala
@@ -4,17 +4,61 @@ import play.api.mvc.{Action, Controller}
 import play.api.libs.json._
 import com.google.inject.Inject
 import org.neo4j.graphdb.{GraphDatabaseService, Node}
-
 import lib.ApiController
 
+
+object ActivityQueryBuilder {
+  val whitelist = Map(
+    "status"    -> "status.code",
+    "hierarchy" -> "node.hierarchy"
+  )
+
+  def activities = new StringBuilder("""
+    | START node = node:entities(type='iati-activity')
+    | MATCH node-[:`activity-status`]-status
+  """.stripMargin)
+
+  def where(query: Map[String, Seq[String]]) = {
+    whitelist.flatMap { case (key, property) =>
+      query.get(key).map { parameter =>
+        parameter.mkString(s"$property IN [", ",", "]")
+      }
+    }.mkString("WHERE ", " AND ", "")
+  }
+
+  def count(query: StringBuilder) = new StringBuilder(query.toString) ++= "RETURN COUNT(node) as total"
+
+  def page(query: StringBuilder, start: Int, limit: Int) = {
+
+  }
+}
 
 class Activities @Inject()(val graph: GraphDatabaseService) extends Controller with ApiController {
 
 
+
   def index = Action { implicit request =>
 
+    import ActivityQueryBuilder._
+
     implicit val writes = lib.JsonWriters.DefaultNodeWrites
-    val (options, results) = list("iati-activity")
+
+    val (start, limit) = paging.bindFromRequest.get
+    val totalizer = count(activities ++= where(request.queryString))
+
+    val (options, results) = Map(
+      "start" -> start,
+      "limit" -> limit,
+      "total" -> engine.execute(totalizer.toString).columnAs[Long]("total").toSeq.head
+    ) -> (
+      engine.execute(builder.append(s"""
+        | RETURN   node
+        | ORDER BY node.`iati-identifier`
+        | SKIP     $start
+        | LIMIT    $limit
+      """.stripMargin).toString).columnAs[Node]("node").toSeq
+    )
+
     Ok(Json.obj(
       "options" -> options,
       "results" -> results
@@ -53,6 +97,19 @@ class Organisations @Inject()(val graph: GraphDatabaseService) extends Controlle
   def view(id: String) = Action {
     implicit val writes = lib.JsonWriters.DeepNodeWrites
     Ok(Json.toJson(single("iati-organisation", id)))
+  }
+}
+
+class ParticipatingOrgs @Inject()(val graph: GraphDatabaseService) extends Controller with ApiController {
+
+  def index = Action { implicit request =>
+
+    implicit val writes = lib.JsonWriters.DefaultNodeWrites
+    val (options, results) = list("participating-org", "ref")
+    Ok(Json.obj(
+      "options" -> options,
+      "results" -> results
+    ))
   }
 }
 
