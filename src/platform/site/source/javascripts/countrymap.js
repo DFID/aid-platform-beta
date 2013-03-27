76f7a898d9a2097c6062041bd89795ed1e79ad56
tswiderek <t.swiderek@kainos.com>
2013-03-27 09:27:37 +0000

    OSM replaced with Google Maps on the project summary page for regional projects

--- src/platform/site/source/javascripts/countrymap.js
+++ src/platform/site/source/javascripts/countrymap.js
@@ -12,20 +12,36 @@
             var mapCenter = geocode.geometry.location;
 
             var northEastBound = new google.maps.LatLng(northeast.lat, northeast.lng);
-            var southwestBound = new google.maps.LatLng(southwest.lat, northeast.lng);
+            var southwestBound = new google.maps.LatLng(southwest.lat, southwest.lng);
             var mapCenterPoint = new google.maps.LatLng(mapCenter.lat, mapCenter.lng);
 
-            var bounds = new google.maps.LatLngBounds(southwestBound, northEastBound);
-            bounds.extend(mapCenterPoint);
-
-            var mapOptions = { mapTypeId: google.maps.MapTypeId.ROADMAP };
-            var map =  new google.maps.Map(document.getElementById("countryMap"), mapOptions);
-
-            map.fitBounds(bounds);
-            map.panToBounds(bounds);
+            createMap(northEastBound, southwestBound, mapCenterPoint);
         }).fail(function() { console.log( "error" ); });
+    } else if (countryCode) {
+        // this is a regional project
+        var bounds = regionBounds[countryCode];
+
+        var northEastBound = new google.maps.LatLng(bounds.northeast.lat, bounds.northeast.lng);
+        var southwestBound = new google.maps.LatLng(bounds.southwest.lat, bounds.southwest.lng);
+
+        createMap(northEastBound, southwestBound, '');
     } else {
         $('#countryMap').hide();
         $('#countryMapDisclaimer').hide();
     }
+
 })(this)
+
+function createMap(northEastBound, southwestBound, mapCenterPoint) {
+    var bounds = new google.maps.LatLngBounds(southwestBound, northEastBound);
+
+    if (mapCenterPoint) {
+       bounds.extend(mapCenterPoint);
+    }
+
+    var mapOptions = { mapTypeId: google.maps.MapTypeId.ROADMAP };
+    var map =  new google.maps.Map(document.getElementById("countryMap"), mapOptions);
+
+    map.fitBounds(bounds);
+    map.panToBounds(bounds);
+}
