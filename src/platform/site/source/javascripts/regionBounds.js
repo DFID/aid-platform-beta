f1b777c5228f1b947703c01b80cbbe6310b601cb
James Hughes <james@yobriefca.se>
2013-04-11 10:45:44 +0100

    Added Kosovo upsert action

--- src/platform/site/source/javascripts/regionBounds.js
+++ src/platform/site/source/javascripts/regionBounds.js
@@ -14,4 +14,19 @@ var regionBounds = {
 	789: {northeast: {lat: 53.56097399999999, lng: 141.0153597}, southwest: {lat: -10.9944991, lng: 73.4994136}},
 	798: {northeast: {lat: 81.669178, lng: -168.3545}, southwest: {lat: -12.9403, lng: 25.0159001}},
 	889: {northeast: {lat: 20.6584862, lng: -124.5410156}, southwest: {lat: -54.83376579999999, lng: 110.9510339}}
+	'AB':{northeast: {lat: 34.307144, lng:137.798767}, southwest: {lat:-10.314919,lng:59.136658}}
+	'BL':{northeast: {lat: 48.327039, lng: 30.956039}, southwest: {lat:39.89288,lng:13.487778}}
+	'CP':{northeast: {lat:5.878332, lng:44.861298}, southwest: {lat:-17.602139,lng:10.05661}}
+	'EA':{northeast: {lat:3.995781, lng:58.835907}, southwest: {lat:-19.601194,lng:23.45993}}
+	'EB':{northeast: {lat:1.45004, lng:44.535141}, southwest: {lat:-10.487812,lng:26.517563}}
+	'ED':{northeast: {lat:48.136767, lng:32.560043}, southwest: {lat:39.622615,lng:14.916}}
+	'EF':{northeast: {lat:48.443778, lng:32.823715}, southwest: {lat:39.943436,lng:15.311508}}
+	'FA':{northeast: {lat:17.434511, lng:19.636688}, southwest: {lat:-6.446318,lng:-15.827179}}
+	'IB':{northeast: {lat:30.902225, lng:148.257751}, southwest: {lat:-14.689881,lng:77.242126}}
+	'LE':{northeast: {lat:-15.617747, lng:-5.185761}, southwest: {lat:-16.324093,lng:-6.298127}}
+	'OT':{northeast: {lat:-15.617747, lng:-5.185761}, southwest: {lat:-16.324093,lng:-6.298127}}
+	'SF':{northeast: {lat:31.353637, lng:56.324158}, southwest: {lat:-14.944785,lng:-15.306702}}
+	'SQ':{northeast: {lat:21.371244, lng:128.796844}, southwest: {lat:-2.460181,lng:93.508759}}
+	'XA':{northeast: {lat:13.667338, lng:40.030746}, southwest: {lat:1.911267,lng:22.254868}}
+
 }
