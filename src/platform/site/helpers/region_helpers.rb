de9249f6b12b789c549165d2750fc988e73981fd
James Hughes <james@yobriefca.se>
2013-04-18 08:56:35 +0100

    Made the country and regional lists smaller and really long

--- src/platform/site/helpers/region_helpers.rb
+++ src/platform/site/helpers/region_helpers.rb
@@ -3,8 +3,11 @@ module RegionHelpers
   # returns an alphabetically sorted list of all regions
   def region_list
 
+    # first get all the region codes we care about
+    relevant_region_codes = @cms_db['projects'].find({ 'projectType' => 'regional' }, :fields => ['recipient']).to_a.map { |c| c['recipient'] }
+
     # get all regions
-    all_regions = @cms_db['regions'].find({})
+    all_regions = @cms_db['regions'].find({'code' => { '$in' => relevant_region_codes }})
 
     # sort them alphabetically (irrespective of case)
     all_regions.sort_by { |region| region['name'][0].upcase }
