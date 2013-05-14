23a4ad57ac1882112a1b1031a0a954dcf9336fb2
James Hughes <james@yobriefca.se>
2013-05-14 19:08:49 +0100

    readded teh git push for pen testing

--- src/platform/build-site.sh
+++ src/platform/build-site.sh
@@ -6,9 +6,9 @@ cd site
 # remove any current build folder and recreate empty
 rm -rf build && mkdir build
 
+# TODO 2013-05-14 James Hughes - Removed this as we are operating under in standalone mode
 # move into build folder and clone current build repo
 git clone git@github.com:DFID/aid-platform-site.git build/
-
 rm -rf build/*
 
 # generate site with middleman
@@ -22,13 +22,13 @@ if [ "$DFID_STATIC_FILE_PATH" != "" ]; then
     cp -fR ./build/* $DFID_STATIC_FILE_PATH
 fi
 
-# commit generated site
 # TODO 2013-05-14 James Hughes - Removed this as we are operating under in standalone mode
-# cd build
-# git add -A
-# git commit -m "Automated Commit through build-site.sh"
-# git push -f origin master
-# cd ..
+# commit generated site
+cd build
+git add -A
+git commit -m "Automated Commit through build-site.sh"
+git push -f origin master
+cd ..
 
 # remove the unnecessary build directory
 rm -rf build
