418e4b879e29acc7a0fc18e34846fa5b4785f0cc
James Hughes <james@yobriefca.se>
2013-05-23 15:54:42 +0100

    Build script now deploys again

--- src/platform/build-site.sh
+++ src/platform/build-site.sh
@@ -8,8 +8,8 @@ rm -rf build && mkdir build
 
 # TODO 2013-05-14 James Hughes - Removed this as we are operating under in standalone mode
 # move into build folder and clone current build repo
-# git clone git@github.com:DFID/aid-platform-site.git build/
-# rm -rf build/*
+git clone git@github.com:DFID/aid-platform-site.git build/
+rm -rf build/*
 
 # generate site with middleman
 bundle install
@@ -24,11 +24,11 @@ fi
 
 # TODO 2013-05-14 James Hughes - Removed this as we are operating under in standalone mode
 # commit generated site
-# cd build
-# git add -A
-# git commit -m "Automated Commit through build-site.sh"
-# git push -f origin master
-# cd ..
+cd build
+git add -A
+git commit -m "Automated Commit through build-site.sh"
+git push -f origin master
+cd ..
 
 # remove the unnecessary build directory
 rm -rf build
