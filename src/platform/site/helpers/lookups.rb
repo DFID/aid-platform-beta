7396b809263f05c2cc2820c220613aa7d0ff137a
Rashed Hoque <r-hoque@dfid.gov.uk>
2013-06-26 13:46:03 +0100

    To handle the issue of 'Overseas Territories'. Neo4j Cypher has been updated. Code logic to render has also been updated.

--- src/platform/site/helpers/lookups.rb
+++ src/platform/site/helpers/lookups.rb
@@ -7,5 +7,9 @@ module Lookups
   def region_name(code)
     (@cms_db['regions'].find_one({ 'code' => code }) || { "name" => "" })["name"]
   end
+
+  def region_name_return_input_when_not_found(code)
+      (@cms_db['regions'].find_one({ 'code' => code }) || { "name" => code })["name"]
+  end
   
 end
