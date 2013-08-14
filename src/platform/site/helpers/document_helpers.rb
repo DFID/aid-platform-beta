b680f4a3056906547daa1bf60e2ad9aa790d73df
R-Hoque <hoque.rashed@gmail.com>
2013-08-15 02:10:12 +0600

    Changes made to show research documents. Reason of this commit is to share the progress through deploying in dev box

---
+++ src/platform/site/helpers/document_helpers.rb
@@ -0,0 +1,27 @@
+require "rubygems"
+require "json"
+require "net/http"
+require "uri"
+ 
+def r4DApiDocFetch(projectId)
+	
+	uri = URI.parse("http://linked-development.org/openapi/r4d/get/research_outputs/"+projectId+"?per_project=10&format=json")
+ 
+	http = Net::HTTP.new(uri.host, uri.port)
+	request = Net::HTTP::Get.new(uri.request_uri)
+ 
+	response = http.request(request)
+ 
+	if response.code == "200"
+		result = JSON.parse(response.body)['results']		
+	else
+		''
+	end
+	
+end
+
+
+ 
+
+
+
