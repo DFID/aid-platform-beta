c5bd4d8881efaeb1355b2dd74ed2529a20074ee1
R-Hoque <hoque.rashed@gmail.com>
2013-08-24 01:31:43 +0600

    Exception handled. If network connection is not available during site build, build process will continue without any error, however r4d docs won't be available.

--- src/platform/site/helpers/document_helpers.rb
+++ src/platform/site/helpers/document_helpers.rb
@@ -5,18 +5,24 @@ require "uri"
  
 def r4DApiDocFetch(projectId)
 	
-	uri = URI.parse("http://linked-development.org/openapi/r4d/get/research_outputs/"+projectId+"?per_project=5&format=json")
+	begin
+	
+		uri = URI.parse("http://linked-development.org/openapi/r4d/get/research_outputs/"+projectId+"?per_project=5&format=json")
  
-	http = Net::HTTP.new(uri.host, uri.port)
-	request = Net::HTTP::Get.new(uri.request_uri)
+		http = Net::HTTP.new(uri.host, uri.port)
+		request = Net::HTTP::Get.new(uri.request_uri)
  
-	response = http.request(request)
+		response = http.request(request)
  
-	if response.code == "200"
-		result = JSON.parse(response.body)['results']		
-	else
+		if response.code == "200"
+			result = JSON.parse(response.body)['results']		
+		else
+			''
+		end
+
+	rescue SystemCallError
 		''
-	end	
+	end
 end
 
 def getR4DSearchLink(link)
