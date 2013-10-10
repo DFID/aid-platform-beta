ce944e9965bef010eee6b5d2d226f98b18cb063b
R-Hoque <hoque.rashed@gmail.com>
2013-10-10 19:27:59 +0600

    Fixed the 404 issue for FCO projects when documents tab clciked.

--- src/platform/site/helpers/document_helpers.rb
+++ src/platform/site/helpers/document_helpers.rb
@@ -6,8 +6,8 @@ require "uri"
 def r4DApiDocFetch(projectId)
 	
 	begin
-	
-		uri = URI.parse("http://linked-development.org/openapi/r4d/get/research_outputs/"+projectId+"?per_project=5&format=json")
+		uri_str = URI.escape("http://linked-development.org/openapi/r4d/get/research_outputs/"+projectId+"?per_project=5&format=json")
+		uri = URI.parse(uri_str)
  
 		http = Net::HTTP.new(uri.host, uri.port)
 		request = Net::HTTP::Get.new(uri.request_uri)
@@ -26,12 +26,14 @@ end
 
 def getR4DSearchLink(link)
 	
-	uri = URI.parse(link)
+	uri_str = URI.escape(link)
+	uri = URI.parse(uri_str)
 	proj_id = uri.path.gsub!(/\D/, "")
 
 	search_uri = "http://r4d.dfid.gov.uk/Search/SearchResults.aspx?search=advancedsearch&SearchType=3&Projects=false&Documents=true&DocumentsOnly=true&ProjectID="+proj_id
 
 	search_uri
+	
 end
 
 def getR4DDocsCountNotShowing(output_count)
