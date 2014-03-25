3b715cb719d3bc489a90bcd0a6ecfb5f143b6e41
Trevor Rea <T.Rea@kainos.com>
2014-03-25 13:28:08 +0000

    Changes required by Trevor for new deployment process.

--- src/platform/build-site.sh
+++ src/platform/build-site.sh
@@ -1,34 +1,17 @@
 #!/bin/bash
 
 # move into correct directory context
-cd site
+cd /opt/generator
+
+# remove backup web directory
+rm -Rf /var/www-bak
 
 # remove any current build folder and recreate empty
-rm -rf build && mkdir build
-
-# TODO 2013-05-14 James Hughes - Removed this as we are operating under in standalone mode
-# move into build folder and clone current build repo
-git clone git@github.com:DFID/aid-platform-site.git build/
-rm -rf build/*
+rm -Rf build && mkdir build
 
 # generate site with middleman
 bundle install
 bundle exec 'middleman build --clean --verbose'
-
-# if the DFID_STATIC_FILE_PATH variable is set then we can
-# deploy the site locally
-if [ "$DFID_STATIC_FILE_PATH" != "" ]; then
-    find $DFID_STATIC_FILE_PATH -mindepth 1 -delete
-    cp -fR ./build/* $DFID_STATIC_FILE_PATH
-fi
-
-# TODO 2013-05-14 James Hughes - Removed this as we are operating under in standalone mode
-# commit generated site
-cd build
-git add -A
-git commit -m "Automated Commit through build-site.sh"
-git push -f origin master
-cd ..
-
-# remove the unnecessary build directory
-rm -rf build
+cp -R /var/www /var/www-bak
+find /var/www -mindepth 1 -delete
+cp -fR ./build/* /var/www
