8b64eb2255461918cb8b7cdd8a14ef741f61819e
Trevor Rea <T.Rea@kainos.com>
2014-07-01 14:06:18 +0100

    Update build-site script

--- src/platform/build-site.sh
+++ src/platform/build-site.sh
@@ -2,16 +2,20 @@
 
 # move into correct directory context
 cd /opt/generator
+source /etc/profile.d/rbenv.sh
+
+# Set ENV['HOME']
+export HOME=/root
 
 # remove backup web directory
 rm -Rf /var/www-bak
 
 # remove any current build folder and recreate empty
-rm -Rf build && mkdir build
+rm -Rf /opt/generator/build && mkdir /opt/generator/build
 
 # generate site with middleman
-bundle install
-bundle exec 'middleman build --clean --verbose'
-cp -R /var/www /var/www-bak
+rbenv exec bundle install
+rbenv exec bundle exec 'middleman build --clean --verbose'
+cp /var/www /var/www-bak
 find /var/www -mindepth 1 -delete
-cp -fR ./build/* /var/www
+cp -fR /opt/generator/build/* /var/www
